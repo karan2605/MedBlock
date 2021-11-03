4859af2dd602634c3bcef80b7dba6725a88d9498
Karan Patel <karan2605@hotmail.co.uk>
2021-11-03 16:46:24 +0000

    setting up development environment

---
+++ client/components/Layout.js
@@ -0,0 +1,13 @@
+export const Center = ({ children, style }) => (
+    <div style={style}>
+      {children}
+  
+      <style jsx>{`
+        div {
+          max-width: 1000px;
+          margin: 0 auto;
+          padding: 20px;
+        }
+      `}</style>
+    </div>
+  )
---
+++ contracts/CreateUser.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract CreateUser {
+    mapping(uint => Profile) profiles;
+
+    struct Profile {
+        uint id;
+        bytes32 username;
+    }
+
+    uint latestUserId = 0;
+
+    function createUser(bytes32 _username) public returns(uint) {
+        latestUserId++;  
+
+        profiles[latestUserId] = Profile(latestUserId, _username);
+
+        return latestUserId;
+    }
+
+}
