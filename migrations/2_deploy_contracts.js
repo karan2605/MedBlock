d38f2144425640c85617e546513e96acddbd7541
Karan Patel <karan2605@hotmail.co.uk>
2022-02-08 16:45:04 +0000

    updated abi's for new contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 const CreateAccount = artifacts.require("CreateAccount");
+const VerifyData = artifacts.require("VerifyData")
 
 module.exports = function(deployer) {
     deployer.deploy(CreateAccount);
+    deployer.deploy(VerifyData);
 }
