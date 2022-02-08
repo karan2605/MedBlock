e55c9b7fe9a2c3a6c1d64606b120c8bb46013854
Karan Patel <karan2605@hotmail.co.uk>
2022-02-08 21:22:05 +0000

    removed functions no longer needed

--- src/contracts/VerifyData.sol
+++ src/contracts/VerifyData.sol
@@ -8,20 +8,4 @@ contract VerifyData {
 
     address[] public validators;
 
-    function addValidators(string[] memory names, string memory place) public returns(address[] memory) {
-        CreateAccount worker = CreateAccount(msg.sender);
-
-        for (uint i = 0; i < names.length; i++) {
-            validators.push(worker.returnUserId(place, names[i]));
-        }
-        return validators;
-    }
-
-    function verifyData() public {
-        
-    }
-
-
-
-
 }
