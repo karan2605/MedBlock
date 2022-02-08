d380ca14fdd0913b6f723bcf058e361d4b18b2a2
Karan Patel <karan2605@hotmail.co.uk>
2022-02-08 16:45:19 +0000

    added new pages

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Createaccount from './CreateAccount/CreateAccount';
 import Login from './Login/Login';
@@ -9,33 +9,55 @@ import WorkerDash from './WorkerDash/WorkerDash';
 import Notifications from './Notifications/Notifications';
 import DataLog from './DataLog/DataLog';
 import AccountSuccess from './CreateAccount/AccountSuccess';
+import PersonalData from './AccessPersonal/PersonalData';
+import MedicalData from './AccessMedical/MedicalData';
+import AddData from './AddData/AddData';
 
-function App() {
-  
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      account: null,
+      firstName: null,
+      lastName: null,
+      dob : null,
+      existingHealth : null,
+      gp : null,
+      appointments : null,
+      notifications : null,
+      requests : null
+    };
+  }
+    
+  render() {
     return (
-      
-      <Router>
-        <div className="App">
-          <div className="content">
-            <Routes>
-              <Route exact path="/" element={<Home />}> </Route>
-              <Route path="/createaccount" element={<Createaccount />}></Route>
-                <Route path="/createaccount/success" element={<AccountSuccess/>}></Route>
-              <Route path="/login" element={<Login />}></Route>
-              <Route path="/logout" element={<Logout />}></Route>
-              <Route path="/patientdash" element={<PatientDash />}></Route>
-                <Route path="/patientdash/notfications" element={<Notifications/>}></Route>
-                <Route path="/patientdash/datalog" element={<DataLog/>}></Route>
-              <Route path="/workerdash" element={<WorkerDash />}></Route>
-                <Route path="/workerdash/notfications" element={<Notifications/>}></Route>
-               
-
-              
-            </Routes>
+        <Router>
+          <div className="App">
+            <div className="content">
+              <Routes>
+                <Route exact path="/" element={<Home />}> </Route>
+                <Route path="/createaccount" element={<Createaccount />}></Route>
+                  <Route path="/createaccount/success" element={<AccountSuccess/>}></Route>
+                <Route path="/login" element={<Login />}></Route>
+                <Route path="/logout" element={<Logout />}></Route>
+                <Route path="/patientdash" element={<PatientDash data={this.state}/>}></Route>
+                  <Route path="/patientdash/notifications" element={<Notifications data={this.state}/>}></Route>
+                  <Route path="/patientdash/datalog" element={<DataLog data={this.state}/>}></Route>
+                  <Route path="/patientdash/personaldata" element={<PersonalData data={this.state}/>}></Route>
+                  <Route path="/patientdash/medicaldata" element={<MedicalData data={this.state}/>}></Route>
+                <Route path="/workerdash" element={<WorkerDash />}></Route>
+                  <Route path="/workerdash/notfications" element={<Notifications data={this.state}/>}></Route>
+                  <Route path="/workerdash/adddata" element={<AddData data={this.state}/>}></Route>
+                  <Route path="/workerdash/personaldata" element={<PersonalData data={this.state}/>}></Route>
+              </Routes>
+            </div>
           </div>
-        </div>
-      </Router>
-    );
+        </Router>
+      );
+    }
   }
   
   export default App;
