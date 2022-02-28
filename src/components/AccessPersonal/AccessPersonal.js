1d71ec031259e4c4e39caae559216eb9a3e6e01e
Karan Patel <karan2605@hotmail.co.uk>
2022-02-28 20:54:38 +0000

    Implementation to allow medical workers to access patient data

--- src/components/AccessPersonal/AccessPersonal.js
+++ src/components/AccessPersonal/AccessPersonal.js
@@ -1,133 +1,166 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
-import { Nav, Button } from 'react-bootstrap';
+import { Nav, Button, Modal } from 'react-bootstrap';
 import Form from 'react-bootstrap/Form';
 import Select from 'react-select';
 import makeAnimated from 'react-select/animated';
+import CreateAccount from '../../abis/CreateAccount.json';
 
 const ipfsClient = require('ipfs-http-client')
 const ipfs = ipfsClient({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' })
+const Web3 = require('web3');
 
 class AccessPersonal extends Component {
 
-  constructor(props) {
-      super(props)
+    async componentDidMount() {
+        await this.loadWeb3()
+        await this.loadBlockchainData()
+    }
 
-      this.state = { 
-          disabled : true,
-          nhsNumber : null,
-          gp : null,
-          bloodGroup : null,
-          existingHealth : null,
-          id : null,
-          firstName : null,
-          lastName : null,
-          dob : null,
-          email : null
-      };
-  }
+    constructor(props) {
+        super(props)
 
-  async findPatient(event) {
-    const getPatientHash = this.state.accountContract.methods.returnNhsToAddr(event.target[0].value).call({from: this.props.data.account})
-    const patientHash = await getPatientHash;
+        this.state = { 
+            showModal : false,
+            accountContract : null,
+            account : null,
+            disabled : true,
+            nhsNumber : null,
+            gp : null,
+            bloodGroup : null,
+            existingHealth : null,
+            id : null,
+            firstName : null,
+            lastName : null,
+            dob : null,
+            email : null,
+            enteredNhs : null
+        };
+    }
 
-    const getHash = this.state.accountContract.methods.getHashByAddr(patientHash).call({from: this.props.data.account})
-    const hash = await getHash
-    const raw_data = await ipfs.cat(hash)
-    const data = JSON.parse(raw_data)
+    async loadWeb3() {
+        if (window.ethereum) {
+            await window.ethereum.request({ method : 'eth_requestAccounts' });
+            window.web3 = new Web3(window.ethereum);
+            return true;
+        }
+        else if (window.web3) {
+            window.web3 = new Web3(window.web3.currentProvider)
+        }
+        else {
+            window.alert('Non-Ethereum browser detected. You should consider trying MetaMask')
+            return false;
+        }
+    }
 
-    this.setState({
-        nhsNumber : data.nhsNumber,
-        gp : data.gp,
-        bloodGroup : data.bloodGroup,
-        existingHealth : data.existingHealth,
-        id : data.account,
-        firstName : data.firstName,
-        lastName : data.lastName,
-        dob : data.dob,
-        email : data.email
-    })
+    async loadBlockchainData() {
+        const web3 = new Web3(window.ethereum);
+        const networkId = await web3.eth.net.getId();
+        const accounts = await web3.eth.getAccounts();
+        this.setState({account: accounts[0]})
+        const networkData = CreateAccount.networks[networkId];
+        if(networkData) {
+            const contract = new web3.eth.Contract(CreateAccount.abi, networkData.address);
+            this.setState({accountContract: contract});
+        } 
+        else {
+            window.alert('Smart contract not deployed to detected network.');
+        }
+    }
 
-    return(this.showData())
-  }
+    async findPatient(event) {
+        event.preventDefault();
 
-  async showData() {
-      
-      return (
-        <div className="d-flex flex-lg-fill justify-content-around p-3 rounded-3 bg-light">
-        <div className="col-lg-5 col-md-5 col-sm-5 container">
-        <h1 className="display-5 fw-bold">Find Patient</h1>
-            <div className="d-flex justify-content-center align-items-center">
-                <div className="p-4 p-lg-5 bg-light rounded-3 flex-fill">
-                    <h1 className="display-5 fw-bold">Medical Data</h1>
-                        <Form>
-                            <Form.Group className="mb-3" controlId="formBasicBlood">
-                                <Form.Label>NHS Number:</Form.Label>
-                                <Form.Control type="text" value={this.state.nhsNumber} name="nhsNumber" disabled/>
-                            </Form.Group>
-                            <fieldset disabled={(this.state.disabled) ? "disabled" : ""}>
-                                <Form.Group className="mb-3" controlId="formBasicGpName">
-                                    <Form.Label>GP:</Form.Label>
-                                    <Form.Control type="text" value={this.state.gp} name="gp"/>
-                                </Form.Group>
+        const getPatientHash = this.state.accountContract.methods.returnNhsToAddr(event.target[0].value).call({from: this.props.data.account})
+        const patientHash = await getPatientHash;
 
-                                <Form.Group className="mb-3" controlId="formBasicBlood">
-                                    <Form.Label>Blood Group:</Form.Label>
-                                    <Form.Control type="text" value={this.state.bloodGroup} name="bloodgroup"/>
-                                </Form.Group>
-                                
-                                
-                                <Form.Group className="mb-3" controlId="formMedicalCond">
-                                    <Form.Label>Existing Health Conditions</Form.Label>
-                                    <Select options={this.options} isMulti closeMenuOnSelect={false} components={makeAnimated()} value={this.props.data.existingHealth} isDisabled={(this.state.disabled) ? "disabled" : ""}/>
-                                </Form.Group>
-                            </fieldset>
-                        </Form>
-                    </div>
-                </div>
-            </div>
+        const getHash = this.state.accountContract.methods.getHashByAddr(patientHash).call({from: this.props.data.account})
+        const hash = await getHash
 
-            <div className="col-lg-5 col-md-5 col-sm-5 container">
-            <div className="d-flex justify-content-center align-items-center">
-                <div className="p-4 p-lg-5 bg-light rounded-3 flex-fill">
-                    <h1 className="display-5 fw-bold">Personal Data</h1>
-                    <Form>
-                        <Form.Group className="mb-3" controlId="formUniqueId">
-                            <Form.Label>Unique Identifier:</Form.Label>
-                            <Form.Control type="text" value={this.state.id} name="Id" disabled/>
-                        </Form.Group>
-                        <fieldset disabled={(this.state.disabled) ? "disabled" : ""}>
-                            <Form.Group className="mb-3" controlId="formBasicFirstName">
-                                <Form.Label>First Name:</Form.Label>
-                                <Form.Control type="text" value={this.state.firstName} name="fname" />
-                            </Form.Group>
+        if (hash) {
+            const raw_data = await ipfs.cat(hash)
+            const data = JSON.parse(raw_data)
 
-                            <Form.Group className="mb-3" controlId="formBasicLastName">
-                                <Form.Label>Last Name:</Form.Label>
-                                <Form.Control type="text" value={this.state.lastName} name="lname" />
-                            </Form.Group>
+            var today = new Date();
+            var date = today.getDate()+'-'+(today.getMonth()+1)+'-'+today.getFullYear();
+            var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+            var dateTime = date+' '+time;
 
-                            <Form.Group controlId="DOB">
-                                <Form.Label>Date of Birth:</Form.Label>
-                                <Form.Control type="date" name="dob" value={this.state.dob} />
-                            </Form.Group>
+            // add access to users medical record
+            const access = [JSON.stringify({
+                datetime : dateTime,
+                accessor : this.props.data.firstName + " " + this.props.data.lastName,
+                type : "Personal and Medical",
+            })]
 
-                            <Form.Group className="mb-3" controlId="formBasicEmail">
-                                <Form.Label>Email:</Form.Label>
-                                <Form.Control type="email" value={this.state.email} name="email" />
-                            </Form.Group>
-                        </fieldset>
-                    </Form>
-                    </div>
-                </div>
-            </div>
-        </div>
-      )
-  }
+            const record = new File([JSON.stringify({
+                id: data.id,
+                firstName: data.firstName,
+                lastName: data.lastName,
+                dob: data.dob,
+                email: data.email,
+                nhsNumber : data.nhsNumber,
+                gp: data.gp,
+                bloodGroup: data.bloodGroup,
+                existingHealth: data.existingHealth,
+                appointments : {
+                    appointment : data.appointment
+                },
+                notifications : {
+                    notification : data.notifications
+                },
+                prescriptions : {
+                    prescription : data.prescription
+                },
+                requests: access.concat(data.requests)
+            })], data.id+".json");
+
+            // send back to ipfs
+            // update hash stored on blockchain
+            ipfs.add(record, (error, result) => {
+                console.log('Ipfs result', result)
+                if(error) {
+                console.error(error)
+                return
+                }
+                this.state.accountContract.methods.setHashbyAddr(result[0].hash, data.id).send({from: this.props.data.account})
+            })
+
+            this.setState({
+                nhsNumber : data.nhsNumber,
+                gp : data.gp,
+                bloodGroup : data.bloodGroup,
+                existingHealth : data.existingHealth,
+                id : data.id,
+                firstName : data.firstName,
+                lastName : data.lastName,
+                dob : data.dob,
+                email : data.email
+            })
+        }
+        else {
+            this.setState({ showModal : true})
+        }
+    }
+
+    handleClose() {
+        this.setState({ showModal : false})
+    }
 
   render() {
     return (  
         <div>
+            <Modal show={this.state.showModal} onHide={this.handleClose.bind(this)}>
+                <Modal.Header closeButton>
+                <Modal.Title>Patient not Found</Modal.Title>
+                </Modal.Header>
+                <Modal.Footer>
+                <Button variant="danger" onClick={this.handleClose.bind(this)}>
+                    Close
+                </Button>
+                </Modal.Footer>
+            </Modal>
+
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                 <a className="navbar-brand" href="#!"><i className="bi bi-box"></i>  MedBlock</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
@@ -151,21 +184,89 @@ class AccessPersonal extends Component {
                     </Nav>
                 </div>
 
-                <div className="d-flex flex-lg-fill justify-content-around p-3 rounded-3 bg-light">
-               
-                    <div className="col-lg-5 col-md-5 col-sm-5 container">
-                    <h1 className="display-5 fw-bold">Find Patient</h1>
-                        <Form onSubmit={this.findPatient}> 
-                            <Form.Group className="mb-3" controlId="formBasicBlood">
-                                <Form.Label>NHS Number:</Form.Label>
-                                <Form.Control type="text" name="nhsNumber"/>
-                            </Form.Group>
+                <div className="col-sm-3 container">
+                    <div className="d-flex justify-content-center align-items-center">
+                        <div className="p-1 p-lg-5 bg-light rounded-3 flex-fill">
+                            <h1 className="display-5 fw-bold">Find Patient</h1>
+                                <Form onSubmit={this.findPatient.bind(this)}> 
+                                    <Form.Group className="mb-3" controlId="formNhsNumber">
+                                        <Form.Label>NHS Number:</Form.Label>
+                                        <Form.Control type="text" name="nhsNumber"/>
+                                    </Form.Group>
 
-                            <Button variant="warning" type="submit">
-                                Find Patient
-                            </Button>
-                        </Form>
+                                    <Button variant="warning" type="submit">
+                                        Find Patient
+                                    </Button>
+                                </Form>
+                            </div>
                     </div>
+                </div>
+
+                <div className="d-flex flex-lg-fill justify-content-around p-3 rounded-3 bg-light">
+                    <div className="col-lg-5 col-md-5 col-sm-5 container">
+                        <div className="d-flex justify-content-center align-items-center">
+                            <div className="p-4 p-lg-5 bg-light rounded-3 flex-fill">
+                                    <h1 className="display-5 fw-bold">Personal Data</h1>
+                                    <Form>
+                                        <Form.Group className="mb-3" controlId="formUniqueId">
+                                            <Form.Label>Unique Identifier:</Form.Label>
+                                            <Form.Control type="text" value={this.state.id} name="Id" disabled/>
+                                        </Form.Group>
+                                        <fieldset disabled={(this.state.disabled) ? "disabled" : ""}>
+                                            <Form.Group className="mb-3" controlId="formBasicFirstName">
+                                                <Form.Label>First Name:</Form.Label>
+                                                <Form.Control type="text" value={this.state.firstName} name="fname" />
+                                            </Form.Group>
+
+                                            <Form.Group className="mb-3" controlId="formBasicLastName">
+                                                <Form.Label>Last Name:</Form.Label>
+                                                <Form.Control type="text" value={this.state.lastName} name="lname" />
+                                            </Form.Group>
+
+                                            <Form.Group controlId="DOB">
+                                                <Form.Label>Date of Birth:</Form.Label>
+                                                <Form.Control type="date" name="dob" value={this.state.dob} />
+                                            </Form.Group>
+
+                                            <Form.Group className="mb-3" controlId="formBasicEmail">
+                                                <Form.Label>Email:</Form.Label>
+                                                <Form.Control type="email" value={this.state.email} name="email" />
+                                            </Form.Group>
+                                        </fieldset>
+                                    </Form>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="col-lg-5 col-md-5 col-sm-5 container">
+                        <div className="d-flex justify-content-center align-items-center">
+                            <div className="p-4 p-lg-5 bg-light rounded-3 flex-fill">
+                            <h1 className="display-5 fw-bold">Medical Data</h1>
+                                    <Form>
+                                        <Form.Group className="mb-3" controlId="formBasicBlood">
+                                            <Form.Label>NHS Number:</Form.Label>
+                                            <Form.Control type="text" value={this.state.nhsNumber} name="nhsNumber" disabled/>
+                                        </Form.Group>
+                                        <fieldset disabled={(this.state.disabled) ? "disabled" : ""}>
+                                            <Form.Group className="mb-3" controlId="formBasicGpName">
+                                                <Form.Label>GP:</Form.Label>
+                                                <Form.Control type="text" value={this.state.gp} name="gp"/>
+                                            </Form.Group>
+
+                                            <Form.Group className="mb-3" controlId="formBasicBlood">
+                                                <Form.Label>Blood Group:</Form.Label>
+                                                <Form.Control type="text" value={this.state.bloodGroup} name="bloodgroup"/>
+                                            </Form.Group>
+                                            
+                                            <Form.Group className="mb-3" controlId="formMedicalCond">
+                                                <Form.Label>Existing Health Conditions</Form.Label>
+                                                <Select options={this.options} isMulti closeMenuOnSelect={false} components={makeAnimated()} value={this.state.existingHealth} isDisabled={(this.state.disabled) ? "disabled" : ""}/>
+                                            </Form.Group>
+                                        </fieldset>
+                                    </Form>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
