dc8f71ec54f346675688ac66c7a8150c12aa48eb
Karan Patel <karan2605@hotmail.co.uk>
2022-03-02 21:02:01 +0000

    Implemented modal form to process patient queries

--- src/components/Notifications/NotificationsPatient.js
+++ src/components/Notifications/NotificationsPatient.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
-import { Nav, Table, Button, Modal } from 'react-bootstrap';
+import { Nav, Table, Button, Modal, Form } from 'react-bootstrap';
 import CreateAccount from '../../abis/CreateAccount.json';
 
 const ipfsClient = require('ipfs-http-client')
@@ -18,6 +18,8 @@ class NotificationsPatient extends Component {
         super(props);
 
         this.state = {
+            showModal : false,
+            queried_notification : null,
             account : null,
             contract : null,
             show : false
@@ -97,15 +99,9 @@ class NotificationsPatient extends Component {
                 gp: this.props.data.gp,
                 bloodGroup: this.props.data.bloodGroup,
                 existingHealth: this.props.data.existingHealth,
-                appointments : {
-                    appointment : appointment.concat(this.props.data.appointment)
-                },
-                notifications : {
-                    notification : this.props.data.notifications
-                },
-                prescriptions : {
-                    prescription : this.props.data.prescription
-                },
+                appointments : appointment.concat(this.props.data.appointment),
+                notifications :  this.props.data.notifications,
+                prescriptions : this.props.data.prescription,
                 requests: this.props.data.requests
             })], this.props.data.account+".json");
 
@@ -117,7 +113,6 @@ class NotificationsPatient extends Component {
                 }
                 this.state.contract.methods.setHash(result[0].hash, this.props.data.nhsNumber).send({from: this.props.data.account})
                 .then(this.setState({show : true}))
-                .then(this.showModal)
             })  
         }
 
@@ -140,15 +135,9 @@ class NotificationsPatient extends Component {
                 gp: this.props.data.gp,
                 bloodGroup: this.props.data.bloodGroup,
                 existingHealth: this.props.data.existingHealth,
-                appointments : {
-                    appointment : this.props.data.appointment
-                },
-                notifications : {
-                    notification : this.props.data.notification
-                },
-                prescriptions : {
-                    prescription : prescription.concat(this.props.data.prescription)
-                },
+                appointments : this.props.data.appointment,
+                notifications : this.props.data.notification,
+                prescriptions : prescription.concat(this.props.data.prescription),
                 requests: this.props.data.requests
             })], this.props.data.account+".json");
 
@@ -160,14 +149,13 @@ class NotificationsPatient extends Component {
                 }
                 this.state.contract.methods.setHash(result[0].hash, this.props.data.nhsNumber).send({from: this.props.data.account})
                 .then(this.setState({show : true}))
-                .then(this.showModal)
             })  
         }  
     }
 
     fetchNotifications() {
         const notification_elements = [];
-        const notifications = this.props.data.notifications.notification
+        const notifications = this.props.data.notifications
         
         for (let i = 0; i < notifications.length-1; i++) {
             const notification = JSON.parse(notifications[i])
@@ -178,7 +166,7 @@ class NotificationsPatient extends Component {
                     <td>{notification.category}</td>
                     <td>Validate Appointment with {notification.doctor} at {notification.place}. Notes: {notification.notes}</td>
                     <td><Button variant="success" onClick={() => { this.addData(notification) }}>Validate</Button>
-                    <Button variant="danger" >Query</Button></td>
+                    <Button variant="danger" onClick={() => { this.showQuery(notification) }}>Query</Button></td>
                 </tr>)
             }
             else {
@@ -188,21 +176,112 @@ class NotificationsPatient extends Component {
                     <td>{notification.category}</td>
                     <td>Validate Prescription with {notification.issuedBy} at {notification.pharmacy}. Notes: {notification.medicine}</td>
                     <td><Button variant="success" onClick={() => { this.addData(notification) }}>Validate</Button>
-                    <Button variant="danger" >Query</Button></td>
+                    <Button variant="danger" onClick={() => { this.showQuery(notification) }}>Query</Button></td>
                 </tr>)
             }
         }
 
         return (
             <tbody>
-            {notification_elements}
+                {notification_elements}
             </tbody>
         )
+    }
+
+    async showQuery(notification) {
+        this.setState({ showModal : true, queried_notification : notification})
+    }
+
+    handleModalClose() {
+        this.setState({ showModal : false})
+    }
+
+    async submitQuery(event) {
+        event.preventDefault();
+
+        const notes = event.target[1].values
+        console.log(notes)
+
+        var today = new Date();
+        var date = today.getDate()+'-'+(today.getMonth()+1)+'-'+today.getFullYear();
+        var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+        var dateTime = date+' '+time;
+
+        //add notification to record
+        const notification = [JSON.stringify({
+            datetime : dateTime,
+            category : "Query",
+            patient : this.props.data.firstName + " " + this.props.data.lastName,
+            patient_notification : this.state.queried_notification.category + " at " + this.state.queried_notification.datetime + ". Notes: " + this.state.queried_notification.notes,
+            notes : notes,
+            senderId : this.props.data.account
+        })]
+
+        // need id of worker that sent the notification
+        const getHash = this.state.contract.methods.getHashByAddr(this.state.queried_notification.senderId).call({from: this.props.data.account})
+        const hash = await getHash
+        const raw_data = await ipfs.cat(hash)
+        const data = JSON.parse(raw_data)
+
+        // add notification to record
+        const record = new File([JSON.stringify({
+            id: data.id,
+            firstName: data.firstName,
+            lastName: data.lastName,
+            dob: data.dob,
+            email: data.email,
+            nhsNumber : data.nhsNumber,
+            gp: data.gp,
+            bloodGroup: data.bloodGroup,
+            existingHealth: data.existingHealth,
+            appointments : data.appointment,
+            notifications : notification.concat(data.notifications),
+            prescriptions : data.prescription,
+            requests: data.requests
+        })], data.id+".json");
+
+        // send back to ipfs
+        // update hash stored on blockchain
+        ipfs.add(record, (error, result) => {
+            console.log('Ipfs result', result)
+            if(error) {
+            console.error(error)
+            return
+            }
+            this.state.contract.methods.setHashbyAddr(result[0].hash, data.id).send({from: this.props.data.account})
+            .then(this.handleModalClose())
+        })
     }
 
     render() {
         return (
             <body>
+                <Modal show={this.state.showModal} onHide={this.handleModalClose.bind(this)}
+                size="lg"
+                aria-labelledby="contained-modal-title-vcenter"
+                centered>
+                    <Modal.Header closeButton>
+                    <Modal.Title id="contained-modal-title-vcenter">Query Form</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Form onSubmit={this.submitQuery.bind(this)}> 
+                            <Form.Group className="mb-3" controlId="formQuery">
+                                <Form.Label>Query:</Form.Label>
+                                <Form.Control as="textarea" rows={3} name="query"/>
+                            </Form.Group>
+
+                            <Button variant="warning" type="submit">
+                                Submit Query
+                            </Button>
+                        </Form>
+                    </Modal.Body>
+                    <Modal.Footer>
+                    <Button variant="danger" onClick={this.handleModalClose.bind(this)}>
+                        Close
+                    </Button>
+                    </Modal.Footer>
+                </Modal>
+
                 <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                     <a className="navbar-brand" href="#!"><i className="bi bi-box"></i>  MedBlock</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
