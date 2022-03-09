02f8f76db008c128084cca985ac3254fc8d3a1c3
Karan Patel <karan2605@hotmail.co.uk>
2022-03-09 16:20:01 +0000

    added map from area to nhs number

--- src/contracts/CreateAccount.sol
+++ src/contracts/CreateAccount.sol
@@ -3,13 +3,14 @@ pragma solidity >=0.7.0 <0.9.0;
 
 contract CreateAccount {
     mapping (address => string) public dataHash;
-
     mapping (string => address) public nhsNumtoAddr;
+    mapping (string => string) public areaToNhsNum;
 
-    function setHash(string memory _ipfsHash, string memory nhsNumber) public {
+    function setHash(string memory _ipfsHash, string memory nhsNumber, string memory area) public {
         dataHash[msg.sender] = _ipfsHash;
         nhsNumtoAddr[nhsNumber] = msg.sender;
-    }
+        areaToNhsNum[area] = nhsNumber;
+    } 
 
     function setHashbyAddr(string memory _ipfsHash, address _addr) public {
         dataHash[_addr] = _ipfsHash;
@@ -26,4 +27,9 @@ contract CreateAccount {
     function returnNhsToAddr(string memory nhsNumber) public view returns (address) {
         return nhsNumtoAddr[nhsNumber];
     } 
+
+    function findHashFromArea(string memory area) public view returns (address) {
+        string memory nhsNum = areaToNhsNum[area];
+        return nhsNumtoAddr[nhsNum];
+    }
 }
