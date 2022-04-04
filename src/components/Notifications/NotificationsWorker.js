6e1fffc002a4100675d01f1ce62b941ba82431c0
Karan Patel <karan2605@hotmail.co.uk>
2022-04-04 10:22:23 +0100

    Implement query response for medical workers

--- src/components/Notifications/NotificationsWorker.js
+++ src/components/Notifications/NotificationsWorker.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { Nav, Table, Button } from 'react-bootstrap';
-import VerifyData from '../../abis/VerifyData.json';
+import CreateAccount from '../../abis/CreateAccount.json';
 
 const Web3 = require('web3');
+const ipfsClient = require('ipfs-http-client');
+const ipfs = ipfsClient({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' })
 
 class NotificationsWorker extends Component {
+
+    async componentDidMount() {
+        await this.loadBlockchainData()
+        await this.loadWeb3()
+    }
 
     constructor(props) {
         super(props);
@@ -15,19 +22,80 @@ class NotificationsWorker extends Component {
         }
     }
 
+    async loadWeb3() {
+        if (window.ethereum) {
+            await window.ethereum.request({ method : 'eth_requestAccounts' });
+            window.web3 = new Web3(window.ethereum);
+            return true;
+        }
+        else if (window.web3) {
+            window.web3 = new Web3(window.web3.currentProvider)
+        }
+        else {
+            window.alert('Non-Ethereum browser detected. You should consider trying MetaMask')
+            return false;
+        }
+    }
+
     async loadBlockchainData() {
         const web3 = new Web3(window.ethereum);
         const networkId = await web3.eth.net.getId();
         const accounts = await web3.eth.getAccounts();
         this.setState({account: accounts[0]})
-        const networkData = VerifyData.networks[networkId];
+        const networkData = CreateAccount.networks[networkId];
         if(networkData) {
-            const contract = new web3.eth.Contract(VerifyData.abi, networkData.address);
+            const contract = new web3.eth.Contract(CreateAccount.abi, networkData.address);
             this.setState({contract: contract});
         } 
         else {
             window.alert('Smart contract not deployed to detected network.');
         }
+    }
+
+    async rejectData(not_data) {
+        var today = new Date();
+        var date = today.getDate()+'-'+(today.getMonth()+1)+'-'+today.getFullYear();
+
+        const notification = [JSON.stringify({
+            datetime : date,
+            category : "Query Response",
+            doctor : this.props.data.firstName + " " + this.props.data.lastName,
+            notification : "Query  : " + not_data.notes,
+            senderId : this.props.data.account
+        })]
+
+        const getHash = this.state.contract.methods.getHashByAddr(not_data.senderId).call({from: this.props.data.account})
+        const hash = await getHash
+        const raw_data = await ipfs.cat(hash)
+        const data = JSON.parse(raw_data)
+
+        // add notification to record
+        const record = new File([JSON.stringify({
+            id: data.id,
+            firstName: data.firstName,
+            lastName: data.lastName,
+            dob: data.dob,
+            email: data.email,
+            nhsNumber : data.nhsNumber,
+            gp: data.gp,
+            bloodGroup: data.bloodGroup,
+            existingHealth: data.existingHealth,
+            appointments : data.appointment,
+            notifications : notification.concat(data.notifications),
+            prescriptions : data.prescription,
+            requests: data.requests
+        })], data.id+".json");
+
+        // send back to ipfs
+        // update hash stored on blockchain
+        ipfs.add(record, (error, result) => {
+            console.log('Ipfs result', result)
+            if(error) {
+            console.error(error)
+            return
+            }
+            this.state.contract.methods.setHashbyAddr(result[0].hash, not_data.senderId).send({from: this.props.data.account})
+        })
     }
 
     fetchNotifications() {
@@ -51,7 +119,9 @@ class NotificationsWorker extends Component {
                     <td>{notification.datetime}</td>
                     <td>{notification.category}</td>
                     <td>Validate Query from {notification.patient} for {notification.patient_notification}. Query: {notification.notes}</td>
-                    <td><Button variant="success" onClick={() => { this.addData(notification) }}>Validate</Button></td>
+                    <td><NavLink className="btn btn-success" variant="success" to="/workerdash/adddata"> Follow up </NavLink>
+                    <Button variant="danger" onClick={() => { this.rejectData(notification)
+                        .then(this.props.data.notifications.splice(this.props.data.notifications.indexOf(notification),1))  }}>Reject</Button></td>
                 </tr>)
             }
         }
