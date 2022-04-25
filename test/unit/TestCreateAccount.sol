7d34eb2c1c20e77e041aedc1f2018191529a979d
Karan Patel <karan2605@hotmail.co.uk>
2022-04-25 20:11:02 +0100

    deleted tests

--- test/unit/TestCreateAccount.sol
+++ test/unit/TestCreateAccount.sol
@@ -1,6 +1,16 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity >=0.7.0 <0.9.0;
+
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+import "../../src/contracts/CreateAccount.sol";
 
 contract TestCreateAccount {
+    function testSetHash() public {
+        CreateAccount createAcc = CreateAccount(DeployedAddresses.CreateAccount());
+
+        Assert.equal(createAcc.setHash("QmWWQSuPMS6aXCbZKpEjPHPUZN2NjB3YrhJTHsV4X3vb2t", "0123456789", "Stanmore Medical Centre"), uint(1), "Should set IPFS hash");
+    }
+
     
 }
